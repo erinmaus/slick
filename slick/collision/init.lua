199fa48728bf76f355f4a29e18354b4d4abe9557
Erin Maus <squeak@erinmaus.com>
2024-12-23 08:36:53 -0500

    Initial work on quad trees.

---
+++ slick/collision/init.lua
@@ -0,0 +1,6 @@
+local collision = {
+    quadTree = require("slick.collision.quadTree"),
+    quadTreeNode = require("slick.collision.quadTreeNode")
+}
+
+return collision
--- slick/geometry/init.lua
+++ slick/geometry/init.lua
@@ -1,6 +1,7 @@
 local geometry = {
     triangulation = require("slick.geometry.triangulation"),
     point = require("slick.geometry.point"),
+    rectangle = require("slick.geometry.rectangle"),
     segment = require("slick.geometry.segment")
 }
 
